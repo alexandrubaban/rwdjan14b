0c918579252a124c81ae3809c6bde0e0ea29cbb0
Alexandru Baban <alexandrubaban@gmail.com>
2014-02-27 10:20:38 -0800

    more work

--- js/script.js
+++ js/script.js
@@ -1,7 +1,43 @@
 // CSS for when JavaScript is Enabled
 document.documentElement.className = "js";
 
-// show, hide navigation when JavaScript is Enabled
+
+// add, remove events
+var eventUtility = {
+    addEvent : (function() {
+        if (typeof addEventListener !== "undefined") {
+            return function(obj, evt, fn) {
+                obj.addEventListener(evt, fn, false);
+            };
+        } else {
+            return function(obj, evt, fn) {
+                obj.attachEvent("on" + evt, fn);
+            };
+        }
+    }()),
+    removeEvent : (function() {
+        if (typeof removeEventListener !== "undefined") {
+            return function(obj, evt, fn) {
+                obj.removeEventListener(evt, fn, false);
+            };
+        } else {
+            return function(obj, evt, fn) {
+                obj.detachEvent("on" + evt, fn);
+            };
+        }
+    }())
+};
+
+
+// watch for click event on menu toggle img
+eventUtility.addEvent(
+	document.getElementById('m'), 'click', function() {
+		toggleNav();
+	}
+);
+
+
+// show, hide navigation 
 function toggleNav() {
 	var theNav = document.getElementById("navlist");
 	if ((theNav.style.display === "none") || (theNav.style.display == "")) {
@@ -10,4 +46,3 @@ function toggleNav() {
 		theNav.style.display = "none";
 	}		
 }
-
